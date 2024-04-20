7f524c99c4cd17b1fced8113544c237b4f647534
omid khadem <omid.khadem@speakap.nl>
2024-04-20 08:58:50 +0200

    Tweak installation script

--- speakap/install.sh
+++ speakap/install.sh
@@ -3,17 +3,16 @@
 # Check if Homebrew is already installed
 if ! command -v brew &>/dev/null; then
     echo "Homebrew is not installed. Proceeding with installation..."
+
+    # Install Homebrew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    # Add Homebrew to PATH
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$(whoami)/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 else
     echo "Homebrew is already installed."
-    exit 0
 fi
-
-# Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# Add Homebrew to PATH
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$(whoami)/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Verify Homebrew installation
 if command -v brew &>/dev/null; then
@@ -24,13 +23,14 @@ else
 fi
 
 # Install Git using Homebrew
-echo "Installing Git via Homebrew..."
-brew install git
-
-# Verify Git installation
-if command -v git &>/dev/null; then
+if ! command -v git &>/dev/null; then
+    echo "Installing Git via Homebrew..."
+    brew install git
     echo "Git has been successfully installed."
-else
+
+    if ! command -v git &>/dev/null; then
     echo "Failed to install Git via Homebrew. Please try again."
     exit 1
+else
+    echo "Git already installed"
 fi
