6ab2a677a6efeac2e85a9da8db39a06ea8c531e4
omid khadem <omid.khadem@speakap.nl>
2024-04-30 10:07:05 +0200

    Separate system setup file

--- speakap/setup_system.sh
+++ speakap/setup_system.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-
+# Install Homebrew
 if ! command -v brew &>/dev/null; then
     echo "Homebrew is not installed. Proceeding with installation..."
 
-    # Install Homebrew
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-    # Add Homebrew to PATH
     echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$(whoami)/.zprofile
     eval "$(/opt/homebrew/bin/brew shellenv)"
 
-    # Verify Homebrew installation
     if command -v brew &>/dev/null; then
         echo "Homebrew has been successfully installed."
     else
@@ -22,7 +19,7 @@ else
     echo "Homebrew is already installed."
 fi
 
-
+# Install Git
 if ! command -v git &>/dev/null; then
     echo "Installing Git via Homebrew..."
     brew install git
@@ -36,23 +33,153 @@ else
     echo "Git is already installed"
 fi
 
-if [ ! -d "~/projects/Speakap-API" ]; then
-    echo "Repository is not cloned. Proceeding with cloning..."
-    mkdir "~/projects/Speakap-API"
-    git clone git@github.com:SpeakapBV/Speakap-API.git ~/projects/Speakap-API
+# Install iTerm
+if [ -d "/Applications/iTerm.app" ]; then
+    echo "iTerm2 is already installed."
+else
+    echo "iTerm2 is not installed. Proceeding with installation..."
+
+    echo "Installing iTerm2 via Homebrew..."
+    brew install --cask iterm2
+
+    if [ -d "/Applications/iTerm.app" ]; then
+        echo "iTerm2 has been successfully installed."
+    else
+        echo "Failed to install iTerm2 via Homebrew. Please try again."
+        exit 1
+    fi
 fi
 
+# Install Oh My Zsh
+if [ -d "$HOME/.oh-my-zsh" ]; then
+    echo "Oh My Zsh is already installed."
+else
+    echo "Oh My Zsh is not installed. Proceeding with installation..."
 
+    echo "Installing Oh My Zsh..."
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+    if [ -d "$HOME/.oh-my-zsh" ]; then
+        echo "Oh My Zsh has been successfully installed."
+    else
+        echo "Failed to install Oh My Zsh. Please try again."
+        exit 1
+    fi
+fi
+
+# Install zsh-autosuggestions using Oh My Zsh
+if [ -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
+    echo "zsh-autosuggestions is already installed."
+else
+    echo "zsh-autosuggestions is not installed. Proceeding with installation..."
+
+    echo "Installing zsh-autosuggestions..."
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+    if [ -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
+        echo "zsh-autosuggestions has been successfully installed."
+    else
+        echo "Failed to install zsh-autosuggestions. Please try again."
+        exit 1
+    fi
+fi
+
+# Install Chroma
+if command -v chroma &>/dev/null ; then
+    echo "Chroma is already installed."
+else 
+    echo "Chroma is not installed. Proceeding with installation..."
+    echo "Installing Chroma..."
+    brew install chroma
+
+    if brew list --formula | grep -q "chroma"; then
+        echo "Chroma has been successfully installed"
+    else
+        echo "Failed to install Chroma via Homebrew. Please try again."
+        exit 1
+    fi
+fi
+
+# Install Atuin
+if command -v atuin &>/dev/null ; then
+    echo "Atuin is already installed."
+else
+    echo "Atuin is not installed. Proceeding with installation..."
+
+    echo "Installing Atuin..."
+    curl https://raw.githubusercontent.com/atuinsh/atuin/main/install.sh | bash
+
+    # Verify installation
+    if brew list --cask | grep -q "atuin"; then
+        echo "Atuin has been successfully installed."
+    else
+        echo "Failed to install Atuin via Homebrew. Please try again."
+        exit 1
+    fi
+fi
+
+# Install Bat
+if brew list --formula | grep -q "bat"; then
+    echo "bat is already installed."
+else
+    echo "bat is not installed. Proceeding with installation..."
+    echo "Installing bat via Homebrew..."
+
+    brew install bat
+
+    if brew list --formula | grep -q "bat"; then
+        echo "bat has been successfully installed."
+    else
+        echo "Failed to install bat via Homebrew. Please try again."
+        exit 1
+    fi
+fi
+
+# Install TLDR
+if brew list --formula | grep -q "tldr"; then
+    echo "tldr is already installed."
+else
+    echo "tldr is not installed. Proceeding with installation..."
+    echo "Installing tldr via Homebrew..."
+    brew install tldr
+
+    if brew list --formula | grep -q "tldr"; then
+        echo "tldr has been successfully installed."
+    else
+        echo "Failed to install tldr via Homebrew. Please try again."
+        exit 1
+    fi
+fi
+
+# Install PHP 8.2
+if brew list --formula | grep -q "php@8.2"; then
+    echo "PHP 8.2 is already installed."
+else
+    echo "PHP 8.2 is not installed. Proceeding with installation..."
+    echo "Installing php8.2 via Homebrew..."
+    brew install php@8.2
+
+    if brew list --formula | grep -q "php@8.2"; then
+        brew install imagemagick
+        brew install pkg-config
+        brew link --force php@8.2
+        pecl install xdebug-3.3.1 imagick redis
+        echo "PHP 8.2 has been successfully installed."
+    else
+        echo "Failed to install PHP 8.2 via Homebrew. Please try again."
+        exit 1
+    fi
+fi
+
+# Install Bitwarden
 if [ -d "/Applications/PhpStorm.app" ]; then
     echo "Bitwarden is already installed."
 else
     echo "Bitwarden is not installed. Proceeding with installation..."
 
-    # Install Bitwarden using Homebrew
     echo "Installing Bitwarden via Homebrew..."
     brew install --cask bitwarden
 
-    # Verify installation
     if brew list --cask | grep -q "bitwarden"; then
         echo "Bitwarden has been successfully installed."
     else
@@ -76,17 +203,15 @@ else
     fi
 fi
 
-
+# Install Google Chrome
 if [ -d "/Applications/Google Chrome.app" ]; then
     echo "Google Chrome is already installed."
 else
     echo "Google Chrome is not installed. Proceeding with installation..."
 
-    # Install Google Chrome using Homebrew
     echo "Installing Google Chrome via Homebrew..."
     brew install --cask google-chrome
 
-    # Verify installation
     if [ -d "/Applications/Google Chrome.app" ]; then
         echo "Google Chrome has been successfully installed."
     else
@@ -95,16 +220,15 @@ else
     fi
 fi
 
+# Install Maccy
 if brew list --cask | grep -q "maccy"; then
     echo "Maccy is already installed."
 else
     echo "Maccy is not installed. Proceeding with installation..."
 
-    # Install Maccy using Homebrew
     echo "Installing Maccy via Homebrew..."
     brew install --cask maccy
 
-    # Verify installation
     if brew list --cask | grep -q "maccy"; then
         echo "Maccy has been successfully installed."
     else
@@ -113,150 +237,7 @@ else
     fi
 fi
 
-if [ -d "/Applications/iTerm.app" ]; then
-    echo "iTerm2 is already installed."
-else
-    echo "iTerm2 is not installed. Proceeding with installation..."
-
-    # Install iTerm2 using Homebrew
-    echo "Installing iTerm2 via Homebrew..."
-    brew install --cask iterm2
-    # Verify installation
-    if [ -d "/Applications/iTerm.app" ]; then
-        echo "iTerm2 has been successfully installed."
-    else
-        echo "Failed to install iTerm2 via Homebrew. Please try again."
-        exit 1
-    fi
-fi
-
-if [ -d "$HOME/.oh-my-zsh" ]; then
-    echo "Oh My Zsh is already installed."
-else
-    echo "Oh My Zsh is not installed. Proceeding with installation..."
-    # Install Oh My Zsh
-    echo "Installing Oh My Zsh..."
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    # Verify installation
-    if [ -d "$HOME/.oh-my-zsh" ]; then
-    git clone https://github.com/zplug/zplug ~/.zplug
-        echo "source ~/.zplug/init.zsh" >> ~/.zshrc
-        echo "zplug \"zplug/zplug\", as:plugin" >> ~/.zshrc
-        echo "zplug load" >> ~/.zshrc
-        source ~/.zshrc
-        echo "Oh My Zsh has been successfully installed."
-    else
-        echo "Failed to install Oh My Zsh. Please try again."
-        exit 1
-    fi
-fi
-
-# Check if zsh-autosuggestions is already installed
-if [ -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
-    echo "zsh-autosuggestions is already installed."    exit 0
-else
-    echo "zsh-autosuggestions is not installed. Proceeding with installation..."
-
-    # Install zsh-autosuggestions using Oh My Zsh
-    echo "Installing zsh-autosuggestions..."
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-    # Verify installation
-    if [ -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
-        echo "zsh-autosuggestions has been successfully installed."
-    else
-        echo "Failed to install zsh-autosuggestions. Please try again."
-        exit 1
-    fi
-fi
-
-
-if command -v chroma &>/dev/null ; then
-    echo "Chroma is already installed."
-else 
-    echo "Chroma is not installed. Proceeding with installation..."
-    echo "Installing Chroma..."
-    brew install chroma
-
-    if brew list --formula | grep -q "chroma"; then
-        echo "Chroma has been successfully installed"
-    else
-        echo "Failed to install Chroma via Homebrew. Please try again."
-        exit 1
-    fi
-fi
-
-if command -v atuin &>/dev/null ; then
-    echo "Atuin is already installed."
-else
-    echo "Atuin is not installed. Proceeding with installation..."
-
-    echo "Installing Atuin..."
-    curl https://raw.githubusercontent.com/atuinsh/atuin/main/install.sh | bash
-
-    # Verify installation
-    if brew list --cask | grep -q "atuin"; then
-        echo "Atuin has been successfully installed."
-    else
-        echo "Failed to install Atuin via Homebrew. Please try again."
-        exit 1
-    fi
-fi
-
-if brew list --formula | grep -q "bat"; then
-    echo "bat is already installed."
-else
-    echo "bat is not installed. Proceeding with installation..."
-    echo "Installing bat via Homebrew..."
-
-    brew install bat
-
-    # Verify installation
-    if brew list --formula | grep -q "bat"; then
-        echo "bat has been successfully installed."
-    else
-        echo "Failed to install bat via Homebrew. Please try again."
-        exit 1
-    fi
-fi
-
-# Install tldr using Homebrew
-if brew list --formula | grep -q "tldr"; then
-    echo "tldr is already installed."
-else
-    echo "tldr is not installed. Proceeding with installation..."
-    echo "Installing tldr via Homebrew..."
-    brew install tldr
-
-    # Verify installation
-    if brew list --formula | grep -q "tldr"; then
-        echo "tldr has been successfully installed."
-    else
-        echo "Failed to install tldr via Homebrew. Please try again."
-        exit 1
-    fi
-fi
-
-if brew list --formula | grep -q "php@8.2"; then
-    echo "PHP 8.2 is already installed."
-else
-    echo "PHP 8.2 is not installed. Proceeding with installation..."
-    echo "Installing php8.2 via Homebrew..."
-    brew install php@8.2
-
-    # Verify installation
-    if brew list --formula | grep -q "php@8.2"; then
-        brew install imagemagick
-        brew install pkg-config
-        brew link --force php@8.2
-        pecl install xdebug-3.3.1 imagick
-        echo "PHP 8.2 has been successfully installed."
-    else
-        echo "Failed to install PHP 8.2 via Homebrew. Please try again."
-        exit 1
-    fi
-fi
-
+# Install Another Redis Desktop Manager
 if brew list --cask | grep -q "another-redis-desktop-manager"; then
     echo "Another Redis Desktop Manager is already installed."
 else
@@ -273,6 +254,7 @@ else
     fi
 fi
 
+# Install DBeaver
 if brew list --cask | grep -q "dbeaver-community"; then
     echo "DBeaver is already installed."
 else
@@ -289,6 +271,7 @@ else
     fi
 fi
 
+# Install VLC
 if brew list --cask | grep -q "vlc"; then
     echo "VLC is already installed."
 else
@@ -305,6 +288,7 @@ else
     fi
 fi
 
+# Install Folx
 if brew list --cask | grep -q "folx"; then
     echo "Folx is already installed."
 else
@@ -321,6 +305,7 @@ else
     fi
 fi
 
+# Install Postman
 if brew list --cask | grep -q "postman"; then
     echo "Postman is already installed."
 else
@@ -337,6 +322,7 @@ else
     fi
 fi
 
+# Install devToys
 if brew list --cask | grep -q "devtoys"; then
     echo "devToys is already installed."
 else
